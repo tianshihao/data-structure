b132a44018af2b1ef7e2cbb20915c1c83043cf65
tianshihao <nd2brth@126.com>
2020-05-11 17:58:18 +0800

    finish base of adjacency list of graph

---
+++ chapter_7/algraph.h
@@ -0,0 +1,36 @@
+﻿#include "predefconst.h"
+#include <stdio.h>
+
+#define MAX_VERTEX_NUM 20
+
+typedef int InfoType;
+
+typedef int VertexType;
+
+typedef enum GraphType
+{
+    DG,
+    DN,
+    UDG,
+    UDN
+} GraphType;
+
+typedef struct ArcNode
+{
+    int adjvex;
+    struct ArcNode *nextarc;
+    InfoType *info;
+} ArcNode;
+
+typedef struct VNode
+{
+    VertexType data;
+    ArcNode *firstarc;
+} VNode, AdjList[MAX_VERTEX_NUM];
+
+typedef struct ALGraph
+{
+    AdjList vertices;
+    int vexnum, arcnum;
+    GraphType type;
+} ALGraph;
