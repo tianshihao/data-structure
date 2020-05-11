fe379b7408fcedd09fc1372f9c9243cd9b42d6fb
tianshihao <nd2brth@126.com>
2020-05-11 14:28:32 +0800

    prepare for adjacency matrix of graph

---
+++ chapter_7/predefconst.h
@@ -0,0 +1,21 @@
+/**
+ * @file:predefconst.h
+ * @brief:
+ * @author:tianshihao
+ * @date:2020-5-11
+ */
+
+#pragma once 
+
+// 函数结果状态代码
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+#define UNDERFLOW -3
+
+// Status 是函数的类型, 其值是函数结果状态代码
+typedef int Status;
+
