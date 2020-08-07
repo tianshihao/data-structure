a4909684ed4183f958b54d7ae6c4328ed2d378b7
tianshihao <tiansh60311@126.com>
2020-08-07 21:00:59 +0800

    feat(bitree): add binary tree

--- include/chapter6/bitree.h
+++ include/chapter6/bitree.h
@@ -1,178 +1,84 @@
-﻿/*******************************************************************************
+﻿/**
  * @file  : bitree.h
- * @brief : 二叉树的二叉链表存储表示
+ * @brief : binary tree header.
  * @author: 田世豪
  * @date  : 2020-2-11
- * ****************************************************************************/
+ * */
 
 #pragma once
 
-#include "predefconst.h"
-#include "sqstack.h"
+#include <chapter1/status.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef int ElemType;
+
+/**
+ * @brief 二叉树结点.
+ */
 typedef struct BiTNode
 {
-    ElemType data;
+    ElemType data; // data field.
     struct BiTNode *lchild, *rchild;
 } BiTNode, *BiTree;
 
-// 构造一个空的二叉树 T.
-Status InitTree(BiTree *T)
-{
-    // *T 即为指向二叉树根结点的指针.
-    if (*T == NULL) // 若已经指向空, 操作失败, 报错.
-    {
-        return ERROR;
-    }
-
-    *T = NULL; // 使得指针指向 NULL.
-
-    return OK;
-} // InitTree
-
-/*******************************************************************************
- * 即使 BiTree 类型和 BiTNode * 等价, 表面上是指针, 但是在指针传递时, 仍要取 BiTree 的地址,
- * 因为在子函数中需要改变是指针本身的内容 (改变了结点间的链接关系), 而不是指针指向的内容, 
- * 因此子函数参数列表中写 BiTree *T, 否则只能传递一份指向某二叉树的指针, 在子函数中修改了二叉树
- * 后, 主函数中的指针仍然指向 NULL.
+/**
+ * @brief 构造一个空的二叉树 T. 其实就是将指向而差的的指针置空.
  * 
- * 简单的理解是, 在 C 语言中, 若想使得传递的参数实际被修改, 则在子函数中加上一层指针总是没有错的,
- * 不论传递的参数是什么, 包括指针.
- * ****************************************************************************/
+ * @param T 指向二叉树
+ * @return OK 操作成功返回 OK.
+ * @return ERROE 操作失败返回 ERROR.
+ */
+Status InitTree(BiTree *T);
 
-void CreateTree(BiTree *T, FILE *fp);
+/**
+  @brief 按先序次序输入二叉树中结点的值, 空格字符表示空树, 构造二叉链表表示的
+ * 二叉树 T. 参考了
+ * 
+ * @param T 指向二叉树的指针.
+ * @param path 存储二叉树序列的 .txt 文件的路径.
+ * @return OK 操作成功返回 OK.
+ * @return ERROR 操作失败返回 ERROR. 
+ */
+Status CreateBiTree(BiTree *T, const char *path);
 
-// 按先序次序输入二叉树中结点的值, 空格字符表示空树,
-// 构造二叉链表表示的二叉树 T.
-// 参考了
-// https://github.com/kangjianwei/Data-Structure/blob/master/Dev-C%2B%2B/CourseBook/0602_BiTree/BiTree.cpp
-Status CreateBiTree(BiTree *T, char *path)
-{
-    FILE *fp = fopen(path, "r");
-    if (fp == NULL)
-    {
-        return ERROR;
-    }
+/**
+ * @brief 从文件中先序次序读取二叉树序列, 递归地建立二叉树.
+ * 
+ * @param T pointer to binary tree.
+ * @param fp 存储二叉树信息的文件.
+ */
+void CreateTree(BiTree *T, FILE *PreSeq);
 
-    // 传递的还是二叉树指针的指针.
-    CreateTree(T, fp);
+/**
+ * @brief 采用二叉链表存储结构, Visit 是对结点操作的应用函数. 先序遍历二叉树 T,
+ * 对每个结点调用函数 Visit 一次且仅一次. 一旦 Visit() 失败, 则操作失败.
+ * 
+ * @param T 非空二叉树.
+ * @param Visit 应用函数.
+ * @return OK 操作成功返回 OK.
+ * @return ERROR 操作失败返回 ERROR.
+ */
+Status PreOrderTraverse(BiTree T, Status (*Visit)(ElemType e));
 
-    fclose(fp);
+/**
+ * @brief 采用二叉表存储结构, Visit 是对结操作的应用函数. 中序遍历二叉 T, 对每个
+ * 结点调用函数 Visit 一次且仅一次. 一旦 Visit() 失败, 则操作失败.
+ * 
+ * @param T 非空二叉树.
+ * @param Visit 应用函数.
+ * @return OK 操作成功返回 OK.
+ * @return ERROR 操作失败返回 ERROR.
+ */
+Status InOrderTaverse(BiTree , Status (*Visit)(ElemType e));
 
-    return OK;
-} // CreateBiTree
-
-// 按先序次序读入字符, 创建二叉树.
-void CreateTree(BiTree *T, FILE *fp)
-{
-    char ch = 0;
-
-    // 读取当前结点的值
-    // if (fp == NULL)
-    // scanf("%c", &ch);
-    // else
-    ch = fgetc(fp);
-
-    if (ch == '^') // 使用'^'表示空结点.
-    {
-        *T = NULL;
-    }
-    else
-    {
-        // 为根结点指针分配要指向要内存空间.
-        *T = (BiTNode *)malloc(sizeof(BiTNode));
-        if (!*T)
-        {
-            exit(OVERFLOW);
-        }
-        
-        (*T)->data = ch;
-        CreateTree(&((*T)->lchild), fp); // 创建左子树
-        CreateTree(&((*T)->rchild), fp); // 创建右子树
-    }
-} // CreateTree
-
-// 采用二叉链表存储结构, Visit 是对结点操作的应用函数.
-// 先序遍历二叉树 T, 对每个结点调用函数 Visit 一次且仅一次.
-// 一旦 Visit() 失败, 则操作失败.
-Status PreOrderTraverse(BiTree T, Status (*Visit)(ElemType e))
-{
-    if (T)
-    {
-        if (Visit(T->data))
-        {
-            if (PreOrderTraverse(T->lchild, Visit))
-            {
-                if (PreOrderTraverse(T->rchild, Visit))
-                {
-                    return OK;
-                }
-            }
-        }
-        return ERROR;
-    }
-    else
-    {
-        return OK;
-    }
-} // PreOrderTraverse
-
-// 采用二叉链表存储结构, Visit 是对结点操作的应用函数.
-// 中序遍历二叉树 T, 对每个结点调用函数 Visit 一次且仅一次.
-// 一旦 Visit() 失败, 则操作失败.
-Status InOrderTraverse(BiTree T, Status (*Visit)(ElemType e))
-{
-    if (T)
-    {
-        if (InOrderTraverse(T->lchild, Visit))
-        {
-            if (Visit(T->data))
-            {
-                if (InOrderTraverse(T->rchild, Visit))
-                {
-                    return OK;
-                }
-            }
-        }
-        return ERROR;
-    }
-    else
-    {
-        return OK;
-    }
-} // InOrderTraverse
-
-// 采用二叉链表存储结构, Visit 是对结点操作的应用函数.
-// 后序遍历二叉树 T, 对每个结点调用函数 Visit 一次且仅一次.
-// 一旦 Visit() 失败, 则操作失败.
-Status PostOrderTraverse(BiTree T, Status (*Visit)(ElemType e))
-{
-    if (T)
-    {
-        if (PostOrderTraverse(T->lchild, Visit))
-        {
-            if (PostOrderTraverse(T->rchild, Visit))
-            {
-                if (Visit(T->data))
-                {
-                    return OK;
-                }
-            }
-        }
-        return ERROR;
-    }
-    else
-    {
-        return OK;
-    }
-} // PostOrderTraverse
-
-// 采用二叉链表存储结构, Visit 是对结点操作的应用函数.
-// 层序遍历二叉树 T, 对每个结点调用函数 Visit 一次且仅一次.
-// 一旦 Visit() 失败, 则操作失败.
-// Status LevelOrderTraverse(BiTree T, Status (*Visit)(ElemType e))
-// {
-//     SqStack S;
-//     InitStack(&S);
-//     Push(&S, )
-// } // LevelOrderTraverse
+/**
+ * @brief 采用二叉链表存储结构, Visit 是对结点操作的应用函数. 后序遍历二叉树 T,
+ * 对每个结点调用函数 Visit 一次且仅一次. 一旦 Visit() 失败, 则操作失败.
+ * 
+ * @param T 非空二叉树.
+ * @param Visit 应用函数.
+ * @return OK 操作成功返回 OK.
+ * @return ERROR 操作失败返回 ERROR.
+ */
+Status PostOrderTraverse(BiTree T, Status (*Visit)(ElemType e));
---
+++ src/chapter6/bitree.c
@@ -0,0 +1,149 @@
+﻿/**
+ * @file bitree.c
+ * @author tianshihao
+ * @brief implementation of binary tree function.
+ * @version 0.1
+ * @date 2020-08-07
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+
+#include <chapter6/bitree.h>
+
+Status InitTree(BiTree *T)
+{
+    // *T 即为指向二叉树根结点的指针.
+    if (*T == NULL) // 若已经指向空, 操作失败, 报错.
+    {
+        return ERROR;
+    }
+
+    *T = NULL; // 使得指针指向 NULL.
+
+    return OK;
+} // InitTree
+
+/**
+ * 即使 BiTree 类型和 BiTNode * 等价, 表面上是指针, 但是在指针传递时, 仍要取 BiTree 的地址,
+ * 因为在子函数中需要改变是指针本身的内容 (改变了结点间的链接关系), 而不是指针指向的内容, 
+ * 因此子函数参数列表中写 BiTree *T, 否则只能传递一份指向某二叉树的指针, 在子函数中修改了二叉树
+ * 后, 主函数中的指针仍然指向 NULL.
+ * 
+ * 简单的理解是, 在 C 语言中, 若想使得传递的参数实际被修改, 则在子函数中加上一层指针总是没有错的,
+ * 不论传递的参数是什么, 包括指针.
+ * */
+
+Status CreateBiTree(BiTree *T, const char *path)
+{
+    // 打开存储二叉树先序序列的文件.
+    FILE *fp = fopen(path, "r");
+
+    // 如果文件打开失败, 返回 ERROR.
+    if (fp == NULL)
+    {
+        return ERROR;
+    }
+
+    // 传递的还是二叉树指针的指针.
+    CreateTree(T, fp);
+
+    fclose(fp);
+
+    return OK;
+} // CreateBiTree
+
+void CreateTree(BiTree *T, FILE *PreSeq)
+{
+    char ch = 0;
+
+    // 读取当前结点的值
+    // if (PreSeq == NULL)
+    // scanf("%c", &ch);
+    // else
+    ch = fgetc(PreSeq);
+
+    if (ch == '^') // 使用'^'表示空结点.
+    {
+        *T = NULL;
+    }
+    else
+    {
+        // 为根结点指针分配要指向要内存空间.
+        *T = (BiTNode *)malloc(sizeof(BiTNode));
+        if (!*T)
+        {
+            exit(OVERFLOW);
+        }
+        
+        (*T)->data = ch;
+        CreateTree(&((*T)->lchild), PreSeq); // 创建左子树
+        CreateTree(&((*T)->rchild), PreSeq); // 创建右子树
+    }
+} // CreateTree
+
+Status PreOrderTraverse(BiTree T, Status (*Visit)(ElemType e))
+{
+    if (T)
+    {
+        if (Visit(T->data))
+        {
+            if (PreOrderTraverse(T->lchild, Visit))
+            {
+                if (PreOrderTraverse(T->rchild, Visit))
+                {
+                    return OK;
+                }
+            }
+        }
+        return ERROR;
+    }
+    else
+    {
+        return OK;
+    }
+} // PreOrderTraverse
+
+Status InOrderTraverse(BiTree T, Status (*Visit)(ElemType e))
+{
+    if (T)
+    {
+        if (InOrderTraverse(T->lchild, Visit))
+        {
+            if (Visit(T->data))
+            {
+                if (InOrderTraverse(T->rchild, Visit))
+                {
+                    return OK;
+                }
+            }
+        }
+        return ERROR;
+    }
+    else
+    {
+        return OK;
+    }
+} // InOrderTraverse
+
+Status PostOrderTraverse(BiTree T, Status (*Visit)(ElemType e))
+{
+    if (T)
+    {
+        if (PostOrderTraverse(T->lchild, Visit))
+        {
+            if (PostOrderTraverse(T->rchild, Visit))
+            {
+                if (Visit(T->data))
+                {
+                    return OK;
+                }
+            }
+        }
+        return ERROR;
+    }
+    else
+    {
+        return OK;
+    }
+} // PostOrderTraverse
